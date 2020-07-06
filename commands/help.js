29ccb75b32a9e41afd23f3f044fff92ece129804
ExillustX <mrexillustx@gmail.com>
2020-07-06 15:13:07 +0100

    update the help command

--- commands/help.js
+++ commands/help.js
@@ -7,6 +7,9 @@ exports.run = (bot, msg, args) => {
       'fields': [{
           'name': process.env.PREFIX + 'help',
           'value': 'List of all commands.'
+        }, {
+          'name': process.env.PREFIX + 'remote',
+          'value': `Remote upload a file. Usage: \`${process.env.PREFIX}remote DIRECT_FILE_URL\``
         }
       ],
       'color': 3264944,
