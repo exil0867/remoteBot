46f0cf2c594d6764d3717028b59067f8d4452838
ExillustX <mrexillustx@gmail.com>
2020-08-01 23:28:44 +0100

    check for max file size before remote uploading

--- commands/remote.js
+++ commands/remote.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
+const fetch = require('node-fetch');
 
-exports.run = (bot, msg, args) => {
+exports.run = async (bot, msg, args) => {
   if (!args[0]) {
     console.log('File URL not found!')
     msg.channel.send('Please add a direct file URL!');
     return;
+  }
+
+  const response = await fetch(args[0]);
+  const fileSizeB = response.headers.get('content-length');
+
+  // If 'content-length' is not null and is a number
+  if (!isNaN(fileSizeB) && fileSizeB !== null) {
+    const fileSizeMB = fileSizeB / Math.pow('1024', 2);
+
+    msg.channel.send(`kb: ${fileSizeB / Math.pow('1024', 1)} MB:${fileSizeMB}`);
+
+    if (fileSizeMB > 100) return msg.channel.send(`Error: File size is larger than 100MB`);
   }
 
   msg.channel.send({
